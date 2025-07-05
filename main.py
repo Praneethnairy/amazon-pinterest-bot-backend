1aa70615b50ee982ededda841e0b8bb21365d987
Praneeth Nairy <praneethnairy@gmail.com>
2025-07-05 22:18:42 +0530

    Sec

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://localhost:3000", "https://amazonpinterestbot.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
